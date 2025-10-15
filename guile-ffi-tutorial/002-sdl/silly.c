9af99efda8d891493f0624a102bd93a61d3abb08
terry <terry.cadd@gmail.com>
2025-10-15 19:24:37 +0100

    added a few more zero argument procedures

--- guile-ffi-tutorial/002-sdl/silly.c
+++ guile-ffi-tutorial/002-sdl/silly.c
@@ -7,8 +7,6 @@
 #include <cairo/cairo.h>
 #include <libguile.h>
 
-
-
 /*
 
 guile   C language
@@ -20,21 +18,73 @@ scheme
 
  */
 
-SCM silly_sdl_wrapper (SCM x)
+SCM zero_wrapper ()
 {
-  return SCM_BOOL_F; //scm_from_double (j0 (scm_to_double (x)));
+  return scm_from_int(0);
 }
 
-void silly_init_sdl ()
+SCM one_wrapper ()
+{
+  return scm_from_int(1);
+}
+
+SCM two_wrapper ()
+{
+  return scm_from_int(2);
+}
+
+SCM three_wrapper ()
+{
+  return scm_from_int(3);
+}
+
+
+SCM falsey_wrapper ()
+{
+  return SCM_BOOL_F; 
+}
+
+SCM truthy_wrapper ()
+{
+  return SCM_BOOL_T; 
+}
+
+// eol = end of list (scheme '())
+SCM eol_wrapper ()
+{
+  return SCM_EOL; 
+}
+
+
+void silly_init ()
 {
   // req = required number of args ?
   // opt = optional number of args ?
   // rst = rest number of args ?
   //SCM scm_c_define_gsubr (const char *name, int req, int opt, int rst, fcn)
-  scm_c_define_gsubr ("init_sdl", 0, 0, 0, silly_sdl_wrapper);
+  scm_c_define_gsubr ("falsey", 0, 0, 0, falsey_wrapper);
+  scm_c_define_gsubr ("truthy", 0, 0, 0, truthy_wrapper);  
+  scm_c_define_gsubr ("empty-list", 0, 0, 0, eol_wrapper);  
+  scm_c_define_gsubr ("zero", 0, 0, 0, zero_wrapper);  
+  scm_c_define_gsubr ("one", 0, 0, 0, one_wrapper);  
+  scm_c_define_gsubr ("two", 0, 0, 0, two_wrapper);
+  scm_c_define_gsubr ("three", 0, 0, 0, three_wrapper);  
+
 }
 
-
+/*
+LTDL_LIBRARY_PATH=./ guile
+(load-extension "libguile-sdl" "silly_init")
+falsey
+truthy 
+(falsey) => #f
+(truthy) => #t
+(empty-list) => ()
+(zero) => 0
+(one) => 1
+(two) => 2
+(three) => 3
+ */
 
 
 
